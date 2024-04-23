ba32646733279396ec8e67e95ee2d158aff1766f
ricksonmenezes <ricksonmenezes@gmail.com>
2024-04-23 12:17:01 +0800

    adding git hub actions

--- catalog-service/src/test/java/com/rgarage/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/rgarage/catalogservice/CatalogServiceApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @Import(ContainersConfig.class)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
