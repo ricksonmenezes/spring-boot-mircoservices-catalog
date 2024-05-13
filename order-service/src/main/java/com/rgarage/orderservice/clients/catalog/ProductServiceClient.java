c3c01ba5c24bcaf3748490aea53bef2ba058d36d
ricksonmenezes <ricksonmenezes@gmail.com>
2024-05-13 14:29:45 +0800

    added retry with fallback

--- order-service/src/main/java/com/rgarage/orderservice/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/com/rgarage/orderservice/clients/catalog/ProductServiceClient.java
@@ -1,5 +1,6 @@
 package com.rgarage.orderservice.clients.catalog;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -20,17 +21,24 @@ public class ProductServiceClient {
         this.restClient = restClient;
     }
 
+    @Retry(name = "catalog-service", fallbackMethod = "getProductByCodeFallBack")
     public Optional<Product> getProductByCode(String code) {
 
-        try {
+
             log.info("fetching product for code {}", code);
             var product = restClient.get().uri("/api/products/{code}", code)
                     .retrieve().body(Product.class);
 
             return Optional.ofNullable(product);
-        } catch (Exception e) {
-            return Optional.empty();
-        }
+
+    }
+
+    Optional<Product> getProductByCodeFallBack(String code, Throwable t) {
+
+        log.error("getProductByCode could not be retried" + code);
+        return Optional.empty();
+
+
     }
 
 }
